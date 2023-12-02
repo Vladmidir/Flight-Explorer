ca614da06209083dfdfefd44ecc2f93bf9a90a61
Nikita Snahovskyy <nikita.snahovskyy@mail.utoronto.ca>
2023-12-01 21:04:28 -0500

    Updated Code organization

--- views/FlightExplorerView.java
+++ views/FlightExplorerView.java
@@ -55,10 +55,11 @@ public class FlightExplorerView {
     // create vbox
 
 
-    Label title;
+    Label title = new Label("Flight Explorer");
     TextField searchBar = new TextField();
 
-    HBox searchBox;
+
+    HBox searchBox = new HBox(this.searchBar);
 
     Scene scene;
 
@@ -71,11 +72,33 @@ public class FlightExplorerView {
 
     WebView webView = new WebView();
 
-    VBox contentBox;
-
-    VBox map;
+    VBox contentBox= new VBox();
 
     WebEngine webEngine = this.webView.getEngine();
+
+    ComboBox<String> comboBox = new ComboBox<>();
+
+    Button searchButton = new Button("Search");
+
+    HBox searchButtonBox = new HBox(this.comboBox, this.searchBox, this.searchButton);
+
+    Label flightTitle = new Label("Flights");
+
+    Label dashboardTitle = new Label("Dashboard");
+
+    ScrollPane scrollPane;
+
+    ScrollPane pinnedPane;
+
+    VBox pinned = new VBox();
+
+    VBox scrolls;
+
+    HBox titleSearchHold;
+
+    /*
+     *
+     */
     public FlightExplorerView(FlightExplorer explorer, Stage stage) { // contructor for view, this is mainly javafx
         this.explorer = explorer;
         this.stage = stage;
@@ -90,39 +113,135 @@ public class FlightExplorerView {
         initUI();
     }
 
+    /*
+     *
+     */
     public void initUI() {
+        createMap();
+        createSearchBar();
+        createComboBox();
+        createSearchButton();
 
-
-        // Load the HTML file into the WebView
-        try {
-            String basePath = System.getProperty("user.dir");
-            String finalPath = basePath + "/views/html/map.html";
-            File htmlFile = new File(finalPath);
-            URI uri = htmlFile.toURI();
-            this.webEngine.load(uri.toURL().toString());
-            this.webEngine.setJavaScriptEnabled(true);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-
-        // Add a listener to wait for the page to finish loading
-        this.webEngine.getLoadWorker().stateProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue == Worker.State.SUCCEEDED) {
-
-
-                String var = "'1'";
-                this.webEngine.executeScript("const number = " + var + ";");
-                this.webEngine.executeScript("showMessage();");
-            }
-        });
-
-
-
-        this.searchBar.setPromptText("Search for a flight");
-        this.searchBox = new HBox(this.searchBar);
         this.searchBox.setAlignment(Pos.TOP_LEFT);
+        this.title.setStyle("-fx-font-size: 28;");
 
+        this.searchButtonBox.setSpacing(15);
+        this.searchButtonBox.setPadding(new Insets(10));
+
+        createTitleSearch(this.title, this.searchButtonBox);
+
+        //Create flight results Box
+        createContentBox();
+        createScrollPane(this.contentBox);
+
+        //Create Pinned Box
+        this.pinned.setSpacing(15);
+        createPinnedPane(this.pinned);
+
+        //set styles for the flight and dashboard titles
+        this.flightTitle.setStyle("-fx-font-size: 28;");
+        this.dashboardTitle.setStyle("-fx-font-size: 28;");
+
+        //create vbox scrolls and set styles
+        createScrolls(this.flightTitle, this.scrollPane, this.dashboardTitle, this.pinnedPane);
+        //create vbox to display
+        createTitleSearchHold(new VBox(this.titleSearch ,this.webView), this.scrolls );
+        //Set the scene
+        this.scene = new Scene(titleSearchHold, 1440, 1000);
+        this.stage.setScene(this.scene);
+        this.stage.show();
+
+    }
+
+    /*
+     *
+     */
+    private void createTitleSearchHold(VBox map, VBox scrolls) {
+
+        this.titleSearchHold = new HBox( map, scrolls);
+        this.titleSearchHold.setAlignment(Pos.TOP_LEFT);
+        this.titleSearchHold.setSpacing(10);
+        this.titleSearchHold.setPadding(new Insets(10));
+    }
+
+    /*
+     *
+     */
+    private void createTitleSearch(Label title, HBox searchButtonBox) {
+
+        this.titleSearch = new VBox(title, searchButtonBox); // ADD MAP HERE
+        this.titleSearch.setAlignment(Pos.TOP_LEFT);
+        this.titleSearch.setPadding(new Insets(25));
+        this.titleSearch.setSpacing(25);
+    }
+
+    /*
+     *
+     */
+    private void performSearch(TextField searchField) {
+        String search = searchField.getText();
+        System.out.println(search);
+        // searched term, search in database
+    }
+
+    /*
+     *
+     */
+    private void createScrolls(Label flightTitle, ScrollPane scrollPane, Label dashboardTitle, ScrollPane pinnedPane) {
+        this.scrolls = new VBox(flightTitle, scrollPane, dashboardTitle, pinnedPane);
+        this.scrolls.setSpacing(40);
+        this.scrolls.setPadding(new Insets(10));
+        this.scrolls.setAlignment(Pos.CENTER);
+    }
+
+    /*
+     *
+     */
+    private void createPinnedPane(VBox pinned) {
+        this.pinnedPane = new ScrollPane(pinned);
+        this.pinnedPane.setPrefViewportHeight(300); // Set the same desired pixel amount
+        this.pinnedPane.setMaxHeight(300);
+        this.pinnedPane.setPrefViewportWidth(500);
+        this.pinnedPane.setMaxWidth(500);
+    }
+
+    /*
+     *
+     */
+    private void createScrollPane(VBox contentBox) {
+        this.scrollPane = new ScrollPane(contentBox);
+        this.scrollPane.setPrefViewportHeight(300); // Set the desired pixel amount
+        this.scrollPane.setMaxHeight(300);
+        this.scrollPane.setPrefViewportWidth(500);
+        this.scrollPane.setMaxWidth(500);
+    }
+
+    /*
+     *
+     */
+    private void createContentBox() {
+        this.contentBox.setSpacing(15);
+        for (int i = 1; i <= 400; i++) { // add labels for flight. #TODO
+            Label label = new Label("Item " + i);
+            this.contentBox.getChildren().add(label);
+        }
+    }
+
+    /*
+     *
+     */
+    private void createSearchButton() {
+        this.searchButton.getStyleClass().add("button");
+        this.searchButton.setStyle("-fx-right: 40px;");
+        this.searchButton.setStyle(" -fx-font-size: 35px;");
+        this.searchButton.setOnAction(e -> performSearch(searchBar));
+    }
+
+    /*
+     *
+     */
+    private void createSearchBar() {
+        this.searchBar.setPromptText("Search for a flight");
         this.searchBar.setPrefWidth(600);
         this.searchBar.setPrefHeight(75);
         this.searchBar.setStyle("-fx-font-size: 28;");
@@ -132,106 +251,53 @@ public class FlightExplorerView {
                 performSearch(searchBar);
             }
         });
+    }
 
-        this.title = new Label("Flight Explorer");
-        this.title.setStyle("-fx-font-size: 28;");
-
-        this.stage.setTitle("FlightExplorer");
-        this.stage.setScene(new Scene(this.grid, 1440, 1000));
-        this.stage.setResizable(true);
-
-        ComboBox<String> comboBox = new ComboBox<>();
+    /*
+     *
+     */
+    private void createComboBox() {
         ObservableList<String> items = FXCollections.observableArrayList(
                 "Flight Number",
                 "Arrival",
                 "Departure",
                 "Free search"
         );
-        comboBox.setItems(items);
-        comboBox.setValue("Select an item");
-        comboBox.setOnAction(e -> {
-            String selectedValue = comboBox.getValue();
+        this.comboBox.setItems(items);
+        this.comboBox.setValue("Select an item");
+        this.comboBox.setOnAction(e -> {
+            String selectedValue = this.comboBox.getValue();
             // return to search to find by type #TODO
         });
-        comboBox.setStyle("-fx-font-size: 26px;");
+        this.comboBox.setStyle("-fx-font-size: 26px;");
+    }
+
+    /*
+     *
+     */
+    private void createMap() {
+        try {
+            // Load the HTML file into the WebView
+            String basePath = System.getProperty("user.dir");
+            String finalPath = basePath + "/views/html/map.html";
+            File htmlFile = new File(finalPath);
+            URI uri = htmlFile.toURI();
+            this.webEngine.load(uri.toURL().toString());
+            this.webEngine.setJavaScriptEnabled(true);
+
+            // Add a listener to wait for the page to finish loading
+            this.webEngine.getLoadWorker().stateProperty().addListener((observable, oldValue, newValue) -> {
+                if (newValue == Worker.State.SUCCEEDED) {
 
 
-        Button searchButton = new Button("Search");
-        searchButton.getStyleClass().add("button");
-        searchButton.setStyle("-fx-right: 40px;");
-        searchButton.setStyle(" -fx-font-size: 35px;");
-        searchButton.setOnAction(e -> performSearch(searchBar));
-
-        HBox searchButtonBox = new HBox(comboBox, this.searchBox, searchButton);
-        searchButtonBox.setSpacing(15);
-        searchButtonBox.setPadding(new Insets(10));
-
-
-        this.titleSearch = new VBox(this.title, searchButtonBox); // ADD MAP HERE
-        this.titleSearch.setAlignment(Pos.TOP_LEFT);
-        this.titleSearch.setPadding(new Insets(25));
-        this.titleSearch.setSpacing(25);
-
-        this.hold = new VBox(this.titleSearch);
-        this.hold.setAlignment(Pos.TOP_LEFT);
-        this.hold.setSpacing(25);
-        this.hold.setPadding(new Insets(25));
-
-
-        this.contentBox = new VBox();
-        this.contentBox.setSpacing(15);
-        for (int i = 1; i <= 400; i++) { // add labels for flight. #TODO
-            Label label = new Label("Item " + i);
-            contentBox.getChildren().add(label);
+                    String var = "'1'";
+                    this.webEngine.executeScript("const number = " + var + ";");
+                    this.webEngine.executeScript("showMessage();");
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        ScrollPane scrollPane = new ScrollPane(contentBox);
-        scrollPane.setPrefViewportHeight(300); // Set the desired pixel amount
-        scrollPane.setMaxHeight(300);
-        scrollPane.setPrefViewportWidth(500);
-        scrollPane.setMaxWidth(500);
-
-        VBox pinned = new VBox();
-        pinned.setSpacing(15);
-        ScrollPane pinnedPane = new ScrollPane(pinned);
-
-        pinnedPane.setPrefViewportHeight(300); // Set the same desired pixel amount
-        pinnedPane.setMaxHeight(300);
-        pinnedPane.setPrefViewportWidth(500);
-        pinnedPane.setMaxWidth(500);
-
-
-        Label flightTitle = new Label("Flights");
-        flightTitle.setStyle("-fx-font-size: 28;");
-        Label dashboardTitle = new Label("Dashboard");
-        dashboardTitle.setStyle("-fx-font-size: 28;");
-
-        VBox scrolls = new VBox(flightTitle, scrollPane, dashboardTitle, pinnedPane);
-        scrolls.setSpacing(40);
-        scrolls.setPadding(new Insets(10));
-        scrolls.setAlignment(Pos.CENTER);
-
-
-
-        this.map = new VBox(this.titleSearch ,this.webView);
-        HBox titleSearchHold = new HBox( this.map, scrolls);
-        titleSearchHold.setAlignment(Pos.TOP_LEFT);
-        titleSearchHold.setSpacing(10);
-        titleSearchHold.setPadding(new Insets(10));
-
-
-        this.scene = new Scene(titleSearchHold, 1440, 1000);
-        this.stage.setScene(this.scene);
-        this.stage.show();
-
     }
-
-    private void performSearch(TextField searchField) {
-        String search = searchField.getText();
-        System.out.println(search);
-        // searched term, search in database
-    }
-
-
-
 
 }
